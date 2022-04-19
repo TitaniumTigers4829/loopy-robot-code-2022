3c3a7d558db1cc817bd5cfedfefa4244548763b1
Traptricker <85963782+Traptricker@users.noreply.github.com>
2022-04-19 00:36:38 -0400

    Decent Changes

--- src/main/java/frc/robot/commands/autonomous/ThreeBallAutoCommand.java
+++ src/main/java/frc/robot/commands/autonomous/ThreeBallAutoCommand.java
@@ -32,9 +32,9 @@ public class ThreeBallAutoCommand extends SequentialCommandGroup {
         // 2. Runs the intake backwards to blow away the ball
         new AutoIntakeBlow(intakeSubsystem).withTimeout(1.5),
 
-        // 3. Slowly approaches the ball while still running the intake backwards
+        // 3. Slowly moves towards the ball while running the intake backwards
         new ParallelCommandGroup(
-            new SetDriveSpeed(driveSubsystem, .5, 0),
+            new SetDriveSpeed(driveSubsystem, .5, 0), // TODO: Tune Values
             new AutoIntakeBlow(intakeSubsystem)
         ).withTimeout(2),
 
@@ -44,7 +44,7 @@ public class ThreeBallAutoCommand extends SequentialCommandGroup {
         new IntakeWithTower(intakeSubsystem, towerSubsystem).withTimeout(1),
 
         // 5. Slowly moves towards the ball while intaking
-        new AutoDriveIntake(driveSubsystem, intakeSubsystem, towerSubsystem, .8).withTimeout(1.2),
+        new AutoDriveIntake(driveSubsystem, intakeSubsystem, towerSubsystem, .8).withTimeout(1.2), // TODO: Tune Values
 
         // 6. Goes back towards the hoop while bumping an enemy ball and revving up the shooter
         new ParallelCommandGroup(
